4b8935025d6af37cfed6ab88713670e9a51b600a
Spyro <epicdudeness677@gmail.com>
2021-07-01 20:14:26 +0200

    Added fade and local movement.

--- Assets/Scripts/DOTweenManager/Editor/DoTweenSettingsDrawer.cs
+++ Assets/Scripts/DOTweenManager/Editor/DoTweenSettingsDrawer.cs
@@ -15,37 +15,22 @@ namespace DOTweenManager.Editor
             SerializedProperty tweenTypeProp = property.FindPropertyRelative("tweenType");
             EditorGUI.PropertyField(pos, tweenTypeProp, new GUIContent(tweenTypeProp.displayName));
             pos.y += pos.height;
-
-            switch (tweenTypeProp.enumValueIndex)
-            {
-                case 0:
-                    DrawElementsContainingPath(ref pos, property, "Movement");
-                    break;
-
-                case 1:
-                    DrawElementsContainingPath(ref pos, property, "Scale");
-                    break;
-
-                case 2:
-                    DrawElementsContainingPath(ref pos, property, "Rotation");
-                    break;
-            }
-
-            SerializedProperty p = property.FindPropertyRelative("playBelowTweenAfterCompletingCurrentTween");
-            EditorGUI.PropertyField(pos, p, new GUIContent(p.displayName));
+            DrawElements(ref pos, property, tweenTypeProp);
             property.serializedObject.ApplyModifiedProperties();
         }
 
-        private void DrawElementsContainingPath(ref Rect pos, SerializedProperty property, string value)
+        private void DrawElements(ref Rect pos, SerializedProperty property, SerializedProperty tweenTypeProp)
         {
+            string value = GetPropertyPathFromTargetTweenType(property);
             SerializedProperty iterator = property.serializedObject.GetIterator();
             iterator.Next(true);
             while (iterator.NextVisible(true))
             {
-                if (iterator.propertyPath.Contains(property.propertyPath) && iterator.propertyPath.Contains(value) &&
+                if (iterator.propertyPath.ToLower().Contains(property.propertyPath.ToLower()) &&
+                    iterator.propertyPath.ToLower().Contains(value) &&
                     DoesNotContainVector3Components(iterator.propertyPath))
                 {
-                    if (value.Equals("Rotation") && iterator.propertyType == SerializedPropertyType.Quaternion)
+                    if (value.Equals("rotation") && iterator.propertyType == SerializedPropertyType.Quaternion)
                     {
                         Vector3 eulerAngles = iterator.quaternionValue.eulerAngles;
                         Vector3 rot = EditorGUI.Vector3Field(pos, new GUIContent(iterator.displayName),
@@ -53,23 +38,70 @@ namespace DOTweenManager.Editor
                                 eulerAngles.z));
                         iterator.quaternionValue = Quaternion.Euler(rot);
                     }
+                    else if (value.Equals("fade") && (
+                        iterator.propertyPath.Contains("uponFadeCompletionDisableGameObject") ||
+                        iterator.propertyPath.Contains("uponFadeCompletionDestroyGameObject")))
+                    {
+                        if (tweenTypeProp.boolValue)
+                        {
+                            EditorGUI.PropertyField(pos, iterator, new GUIContent(iterator.displayName));
+                        }
+                    }
                     else
                         EditorGUI.PropertyField(pos, iterator, new GUIContent(iterator.displayName));
 
                     pos.y += pos.height;
                 }
             }
+
+            SerializedProperty p = property.FindPropertyRelative("playBelowTweenAfterCompletingCurrentTween");
+            EditorGUI.PropertyField(pos, p, new GUIContent(p.displayName));
         }
 
         private bool DoesNotContainVector3Components(string iteratorPropertyPath)
         {
-            return !iteratorPropertyPath.Contains(".x") && !iteratorPropertyPath.Contains(".y") &&
-                   !iteratorPropertyPath.Contains(".z") && !iteratorPropertyPath.Contains(".w");
+            return !iteratorPropertyPath.ToLower().Contains(".x") && !iteratorPropertyPath.ToLower().Contains(".y") &&
+                   !iteratorPropertyPath.ToLower().Contains(".z") && !iteratorPropertyPath.ToLower().Contains(".w");
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            return (base.GetPropertyHeight(property, label) * 5f) + EditorGUIUtility.singleLineHeight;
+            int totalElements = 2;
+            SerializedProperty iterator = property.serializedObject.GetIterator();
+            iterator.Next(true);
+            while (iterator.NextVisible(true))
+            {
+                if (iterator.propertyPath.ToLower().Contains(property.propertyPath.ToLower()) &&
+                    iterator.propertyPath.ToLower().Contains(GetPropertyPathFromTargetTweenType(property)) &&
+                    DoesNotContainVector3Components(iterator.propertyPath))
+                {
+                    totalElements++;
+                }
+            }
+
+            return (base.GetPropertyHeight(property, label) * totalElements) + EditorGUIUtility.singleLineHeight;
+        }
+
+        private string GetPropertyPathFromTargetTweenType(SerializedProperty property)
+        {
+            SerializedProperty tweenType = property.FindPropertyRelative("tweenType");
+            switch (tweenType.enumValueIndex)
+            {
+                case 0:
+                    return "movement";
+
+                case 1:
+                    return "scale";
+
+                case 2:
+                    return "rotation";
+
+                case 3:
+                    return "fade";
+
+                default:
+                    return "";
+            }
         }
     }
 }
